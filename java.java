6464f713d5eaf2d989cd3cc97291cc9dc8bc5558
Tom <tom.vanbraband@sonarsource.com>
2022-05-10 17:15:54 +0200

    Update java.java

--- java.java
+++ java.java
@@ -16,5 +16,7 @@ public class Test {
   private final String TEST2 = "test2";
   private final String TEST2 = "test2";
   private final String TEST2 = "test2";
+  private final String TEST5 = "test5";
+
 
 }
